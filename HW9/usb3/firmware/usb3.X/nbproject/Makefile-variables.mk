304611087a841d3a3c277507a2f01639fcc6b4a9
AveryB <avery.bellis@gmail.com>
2017-06-09 13:39:05 -0500

    Upload HW 9

---
+++ HW9/usb3/firmware/usb3.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=usb3.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/usb3.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=usb3.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/usb3.x.tar
